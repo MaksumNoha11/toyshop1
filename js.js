fb093c0f2243599dc7579446288b01413c52b71b
MaksumNoha11 <noha.nastya@gmail.com>
2025-02-07 17:09:25 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,52 +1,59 @@
-// var car = document.getElementById('car');
-// var startBtn = document.getElementById('startBtn');
-// var stopBtn = document.getElementById('stopBtn');
 
-// var animation;
-// var position = 0;
-// var speed = 5;
-// var isMoving = false;
+    var car = document.getElementById('car');
+    var startBtn = document.getElementById('startBtn');
+    var stopBtn = document.getElementById('stopBtn');
+    var nightModeBtn = document.getElementById('nightModeBtn');
+    var headlight = document.getElementById('headlight');
+    var sun = document.getElementById('sun');
+    var animation;
+    var position = 0;
+    var speed = 30;
+    var isMoving = false;
+    var nightMode = false;
 
-// function moveCar() {
-//     position += speed;
-//     if (position > window.innerWidth) {
-//         position = -365;
-//     }
-//     car.style.left = position + 'px';
-// }
+    function moveCar() {
+        position += speed;
+        if (position > window.innerWidth) {
+            position = -365;
+        }
+        car.style.left = position + 'px';
+        car.style.transform = `translateY(${Math.sin(position / 50) * 5}px)`;
+    }
 
-// function stopMoving() {
-//     clearInterval(animation); 
-//     isMoving = false; 
-// }
+    function stopMoving() {
+        clearInterval(animation);
+        isMoving = false;
+    }
 
-// function startMoving() {
-//     speed += 2;
-//     if (!isMoving) { 
-//         animation = setInterval(moveCar, 50);
-//         isMoving = true;
-//     }
-// }
+    function startMoving() {
+        speed += 2;
+        if (!isMoving) {
+            animation = setInterval(moveCar, 50);
+            isMoving = true;
+        }
+    }
 
-// startBtn.addEventListener('click', startMoving);
-// stopBtn.addEventListener('click', stopMoving);
+    startBtn.addEventListener('click', () => {
+        startMoving();
+    });
 
-// function moveClouds() {
-//     document.querySelectorAll('.cloud').forEach((cloud, index) => {
-//         let cloudPos = parseInt(cloud.style.left) || Math.random() * window.innerWidth;
-//         cloudPos += 5;
+    stopBtn.addEventListener('click', () => {
+        stopMoving();
+    });
 
-//         if (cloudPos > window.innerWidth) {
-//             cloudPos = -150;
-//         }
+    nightModeBtn.addEventListener('click', () => {
+        nightMode = !nightMode;
+        document.body.style.background = nightMode ? '#1a1a2e' : '#3ea1cb';
+        sun.style.top = nightMode ? '500px' : '50px';
 
-//         cloud.style.left = cloudPos + 'px';
-
-//         if (!cloud.style.top || parseInt(cloud.style.top) < 300) {
-//             cloud.style.top = `${300 + index * 50}px`;
-//         }
-//     });
-// }
-
-// setInterval(moveClouds, 50);
+        if (nightMode) {
+            headlight.setAttribute('fill', 'yellow'); // Включаємо фари в нічному режимі
+            document.body.classList.add('night-mode');
+            document.body.classList.remove('day-mode');
+        } else {
+            headlight.setAttribute('fill', 'gold'); // Вимикаємо фари при денному режимі
+            document.body.classList.remove('night-mode');
+            document.body.classList.add('day-mode');
+        }
+    });
 
