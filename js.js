e39aff09068645aa2b2eb662b8883c0cd97841b8
MaksumNoha11 <noha.nastya@gmail.com>
2025-02-07 17:05:41 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,52 +1,52 @@
-var car = document.getElementById('car');
-var startBtn = document.getElementById('startBtn');
-var stopBtn = document.getElementById('stopBtn');
+// var car = document.getElementById('car');
+// var startBtn = document.getElementById('startBtn');
+// var stopBtn = document.getElementById('stopBtn');
 
-var animation;
-var position = 0;
-var speed = 5;
-var isMoving = false;
+// var animation;
+// var position = 0;
+// var speed = 5;
+// var isMoving = false;
 
-function moveCar() {
-    position += speed;
-    if (position > window.innerWidth) {
-        position = -365;
-    }
-    car.style.left = position + 'px';
-}
+// function moveCar() {
+//     position += speed;
+//     if (position > window.innerWidth) {
+//         position = -365;
+//     }
+//     car.style.left = position + 'px';
+// }
 
-function stopMoving() {
-    clearInterval(animation); 
-    isMoving = false; 
-}
+// function stopMoving() {
+//     clearInterval(animation); 
+//     isMoving = false; 
+// }
 
-function startMoving() {
-    speed += 2;
-    if (!isMoving) { 
-        animation = setInterval(moveCar, 50);
-        isMoving = true;
-    }
-}
+// function startMoving() {
+//     speed += 2;
+//     if (!isMoving) { 
+//         animation = setInterval(moveCar, 50);
+//         isMoving = true;
+//     }
+// }
 
-startBtn.addEventListener('click', startMoving);
-stopBtn.addEventListener('click', stopMoving);
+// startBtn.addEventListener('click', startMoving);
+// stopBtn.addEventListener('click', stopMoving);
 
-function moveClouds() {
-    document.querySelectorAll('.cloud').forEach((cloud, index) => {
-        let cloudPos = parseInt(cloud.style.left) || Math.random() * window.innerWidth;
-        cloudPos += 5;
+// function moveClouds() {
+//     document.querySelectorAll('.cloud').forEach((cloud, index) => {
+//         let cloudPos = parseInt(cloud.style.left) || Math.random() * window.innerWidth;
+//         cloudPos += 5;
 
-        if (cloudPos > window.innerWidth) {
-            cloudPos = -150;
-        }
+//         if (cloudPos > window.innerWidth) {
+//             cloudPos = -150;
+//         }
 
-        cloud.style.left = cloudPos + 'px';
+//         cloud.style.left = cloudPos + 'px';
 
-        if (!cloud.style.top || parseInt(cloud.style.top) < 300) {
-            cloud.style.top = `${300 + index * 50}px`;
-        }
-    });
-}
+//         if (!cloud.style.top || parseInt(cloud.style.top) < 300) {
+//             cloud.style.top = `${300 + index * 50}px`;
+//         }
+//     });
+// }
 
-setInterval(moveClouds, 50);
+// setInterval(moveClouds, 50);
 
